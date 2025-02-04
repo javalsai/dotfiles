a40903f375ec3cdaed10e0b0f14cefd53a74d58f
javalsai <jvssxxi@gmail.com>
2025-02-04 16:04:32 +0100

    feat(lualine): word count & selection variant

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,28 @@
+local function plural(str, bool)
+    if bool then
+        return str .. "s"
+    else
+        return str
+    end
+end
+
+local function getWords()
+    if vim.api.nvim_buf_get_offset(0, (vim.fn.getbufinfo(vim.fn.bufnr()))[1].linecount) > 1024 * 1024 then
+        return ""
+    end
+
+    local wordcount = vim.fn.wordcount()
+
+    local words = wordcount.words
+    local text = " word"
+    if wordcount.visual_words ~= nil then
+        text = " selected" .. text
+        words = wordcount.visual_words
+    end
+
+    return plural(tostring(words) .. text, words > 1)
+end
+
 return {
     "nvim-lualine/lualine.nvim",
     name = "lualine",
@@ -7,7 +32,8 @@ return {
     config = {
         extensions = { 'neo-tree', 'mason', 'lazy', 'fugitive' },
         sections = {
-            lualine_c = { { 'filename', path = 1 }, 'selectioncount', 'searchcount' }
+            lualine_c = { { 'filename', path = 1 }, 'selectioncount', 'searchcount' },
+            lualine_x = { getWords }
         },
         inactive_sections = {
             lualine_c = { { 'filename', path = 1 } }
