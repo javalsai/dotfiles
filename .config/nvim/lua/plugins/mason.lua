24fbbf3bf892c6d9f654174521055efe89c58181
javalsai <jvssxxi@gmail.com>
2025-02-06 23:58:34 +0100

    feat(nvim): mason isntall lsps automatically

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,24 @@
 return {
-    "williamboman/mason.nvim",
-    config = true,
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+        {
+            "williamboman/mason.nvim",
+            lazy = false,
+            config = true,
+        }
+    },
+    opts = {
+        ensure_installed = {
+            'asm_lsp',
+            'bashls',
+            'biome',
+            'cssls',
+            'emmet_ls',
+            'lemminx',
+            'remark_ls',
+            'taplo',
+            'ts_ls',
+            'zls',
+        }
+    },
 }
