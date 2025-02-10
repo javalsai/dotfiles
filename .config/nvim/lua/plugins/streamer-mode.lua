9d7e435cd73a51667a9f069efe9ef87cae5e962d
javalsai <jvssxxi@gmail.com>
2025-02-10 19:34:43 +0100

    feat(nvim): stop leaking tokens in discord vc

---
+++ .config/nvim/lua/plugins/streamer-mode.lua
@@ -0,0 +1,59 @@
+return {
+    'Kolkhis/streamer-mode.nvim',
+    config = function()
+        require('streamer-mode').setup
+        {
+            use_defaults = false,
+
+            paths = {
+                '*',
+            },
+
+            keywords = {
+                '\\(\'\\|"\\)*api_key\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*token\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*client_secret\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*powershell\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*$env:\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*name\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*userpassword\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*username\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*user.name\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*user.password\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*user.email\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*email\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*signingkey\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*IdentityFile\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+                '\\(\'\\|"\\)*credential.helper\\(\'\\|"\\)*\\s*\\(=\\|:\\)\\s*....',
+            },
+
+            level = 'edit',
+            default_state = 'on',
+            conceal_char = '…',
+            patterns = {},
+        }
+    end
+    -- opts = {
+    --     default_state = 'on',
+    --     level = 'edit',
+    --     keywords = {
+    --         'api_key',
+    --         'token',
+    --         'client_secret',
+    --         'powershell',
+    --         '$env:',
+    --         'name',
+    --         'userpassword',
+    --         'username',
+    --         'user.name',
+    --         'user.password',
+    --         'user.email',
+    --         'email',
+    --         'signingkey',
+    --         'IdentityFile',
+    --         'credential.helper',
+    --         '!export',
+    --         '!alias',
+    --     },
+    -- }
+}
