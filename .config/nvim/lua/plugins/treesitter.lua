c6f34562d07c2572fb32d590de24b9ce189d24c1
javalsai <jvssxxi@gmail.com>
2025-08-25 21:14:24 +0200

    nvim: add treesitter text objects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,10 @@ local globals = require 'globals'
 
 return {
   'nvim-treesitter/nvim-treesitter',
-  dependencies = { 'nvim-treesitter/nvim-treesitter-context' },
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-context',
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
   config = function()
     require 'nvim-treesitter.configs'.setup {
       ensure_installed = globals.ts_langs,
@@ -12,6 +15,25 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+            ['aP'] = '@parameter.outer',
+            ['iP'] = '@parameter.inner',
+            ['ak'] = '@block.outer',
+            ['ik'] = '@block.inner',
+            ['al'] = '@loop.outer',
+            ['il'] = '@loop.inner',
+            ['acm'] = '@comment.outer',
+            ['icm'] = '@comment.inner',
+          },
+        },
       },
     }
   end,
