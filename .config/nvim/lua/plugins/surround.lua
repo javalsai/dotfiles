6d8adf91928133160d4c21e682b1340e92f06caa
javalsai <jvssxxi@gmail.com>
2025-08-25 21:12:49 +0200

    nvim rename surround

---
+++ .config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,9 @@
+return {
+  'kylechui/nvim-surround',
+  event = 'VeryLazy',
+  config = true,
+}
+
+-- ys<motion><surround>   | ysiw)
+-- ds<surroud>            | ds"
+-- cs<surround><surround> | cs'"
