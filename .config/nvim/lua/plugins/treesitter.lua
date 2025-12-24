c0842319957364cc0fc83e36be161be47e07216d
javalsai <jvssxxi@gmail.com>
2025-12-24 21:24:04 +0100

    update(nvim): update to `vim.lsp`

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,40 +1,50 @@
 local globals = require 'globals'
 
+local function makeTextObj(binding, obj)
+  return {
+    binding,
+    function()
+      require 'nvim-treesitter-textobjects.select'.select_textobject(obj, 'textobjects')
+    end,
+    mode = { 'x', 'o' },
+    desc = 'Treesitter textobj',
+  }
+end
+
 return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-context',
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  config = function()
-    require 'nvim-treesitter.configs'.setup {
-      ensure_installed = globals.ts_langs,
-      ignore_install = {},
-      modules = {},
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      textobjects = {
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      branch = 'main',
+      lazy = false,
+      config = {
         select = {
-          enable = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-            ['aP'] = '@parameter.outer',
-            ['iP'] = '@parameter.inner',
-            ['ak'] = '@block.outer',
-            ['ik'] = '@block.inner',
-            ['al'] = '@loop.outer',
-            ['il'] = '@loop.inner',
-            ['acm'] = '@comment.outer',
-            ['icm'] = '@comment.inner',
-          },
+          lookahead = true,
+          include_surroundings_whitespace = true,
         },
       },
-    }
+
+      keys = {
+        makeTextObj('af', '@function.outer'),
+        makeTextObj('if', '@function.inner'),
+        makeTextObj('ac', '@class.outer'),
+        makeTextObj('ic', '@class.inner'),
+        makeTextObj('aP', '@parameter.outer'),
+        makeTextObj('iP', '@parameter.inner'),
+        makeTextObj('ak', '@block.outer'),
+        makeTextObj('ik', '@block.inner'),
+        makeTextObj('al', '@loop.outer'),
+        makeTextObj('il', '@loop.inner'),
+        makeTextObj('acm', '@comment.outer'),
+        makeTextObj('icm', '@comment.inner'),
+      },
+    },
+  },
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    require 'nvim-treesitter'.install(globals.ts_langs)
   end,
 }
