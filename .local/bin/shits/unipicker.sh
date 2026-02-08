f51b947cf337f5618384f515ce00ff41ceffe7d9
javalsai <jvssxxi@gmail.com>
2026-02-08 03:06:52 +0100

    update: .local/bin scripts
    get-hypr-hover-window now should be more miding of window layering
    unipicker now should use vicinae

--- .local/bin/shits/unipicker.sh
+++ .local/bin/shits/unipicker.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 [ -z "$LANG" ] && export LANG=es_ES.UTF-8
 
 unipicker \
-    --command 'wofi -dmenu 2> /dev/null' \
+    --command 'vicinae dmenu 2> /dev/null' \
     --copy-command cat | tail -n +2
