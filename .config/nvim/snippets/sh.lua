3684aef2b5d3b8e3025e4323fcb15d74c1dbe513
javalsai <jvssxxi@gmail.com>
2026-06-01 05:56:02 +0200

    nvim: add shellcheck snippet

--- .config/nvim/snippets/sh.lua
+++ .config/nvim/snippets/sh.lua
@@ -1,3 +1,4 @@
 return {
-  s('#!shebang', { t({ '#!/usr/bin/env bash', 'set -euo pipefail', '', '' }), i(0) })
+  s('#!shebang', { t({ '#!/usr/bin/env bash', 'set -euo pipefail', '', '' }), i(0) }),
+  s('# shellcheck disable=', { t('# shellcheck disable='), i(0) }),
 }
