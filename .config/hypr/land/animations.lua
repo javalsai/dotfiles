f435e7f982c118078d241c3b5acf09ebd1b61a5a
javalsai <jvssxxi@gmail.com>
2026-06-10 20:12:40 +0200

    hypr/anim: I think I prefer exponential out

--- .config/hypr/land/animations.lua
+++ .config/hypr/land/animations.lua
@@ -1,10 +1,13 @@
 -- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/#animation-tree
 
+-- ease out expo (my eyes easily take 2x the speed from before, feels more responsive without dropping the animation)
+hl.curve('my-default', { type = 'bezier', points = { { 0.16, 1 }, { 0.3, 1 } } })
+
 for _, leaf in ipairs({ 'workspaces', 'windows' }) do
   hl.animation({
     leaf = leaf,
     enabled = true,
-    bezier = 'default',
-    speed = 5,
+    bezier = 'my-default',
+    speed = 2.8,
   })
 end
