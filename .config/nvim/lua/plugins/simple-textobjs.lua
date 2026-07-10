9c31dcdc77802d70f432518a7d80030ad6b44cb2
javalsai <jvssxxi@gmail.com>
2026-07-10 19:41:30 +0200

    nvim(textobjs): add comment summarizing importants

--- .config/nvim/lua/plugins/simple-textobjs.lua
+++ .config/nvim/lua/plugins/simple-textobjs.lua
@@ -1,3 +1,18 @@
+-- reminders:
+-- - subword is S
+-- - argument is ,
+-- - anybracket is o
+-- - any quote is q
+-- - key/value are k/v
+-- - URLs are L
+-- and more misc:
+-- - emoji/glyph is .
+-- - color in CSS is #
+-- - [[...]] is D
+-- - numbers are n
+-- - nvim diagnostic refered text is !
+-- - last change is ;
+
 return {
   'chrisgrieser/nvim-various-textobjs',
   event = 'VeryLazy',
