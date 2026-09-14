836eac3acfa81b26321fe852c53826e3fc2d2bcf
javalsai <jvssxxi@gmail.com>
2026-09-14 03:57:04 +0200

    zsh(antidote): update cache zsh files
    might just stop tracking these if it causes issues between devices

--- .config/zsh/.zsh_plugins.zsh
+++ .config/zsh/.zsh_plugins.zsh
@@ -1,16 +1,15 @@
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git/git.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git-lfs" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git-lfs/git-lfs.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/extract" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/extract/extract.plugin.zsh"
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/directories.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-javalsai-SLASH-fast-syntax-highlighting" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-javalsai-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme"
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git/git.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git-lfs" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git-lfs/git-lfs.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/extract" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/extract/extract.plugin.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/directories.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/javalsai/fast-syntax-highlighting" )
+source "$HOME/.cache/antidote/github.com/javalsai/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-autosuggestions" )
+source "$HOME/.cache/antidote/github.com/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-history-substring-search" )
+source "$HOME/.cache/antidote/github.com/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/romkatv/powerlevel10k" )
+source "$HOME/.cache/antidote/github.com/romkatv/powerlevel10k/powerlevel10k.zsh-theme"
--- .config/zsh/.zsh_plugins_noninteractive.zsh
+++ .config/zsh/.zsh_plugins_noninteractive.zsh
@@ -1,7 +1,7 @@
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git/git.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git-lfs" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git-lfs/git-lfs.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/extract" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/extract/extract.plugin.zsh"
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/directories.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git/git.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git-lfs" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git-lfs/git-lfs.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/extract" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/extract/extract.plugin.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/directories.zsh"
