0e013bb83a7bc7f2205cd08a45bb847cbacd5942
javalsai <jvssxxi@gmail.com>
2026-09-14 04:31:37 +0200

    dotfiles: xdg all the dotfiles git paths
    needs partial manual migration

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -49,7 +49,7 @@ alias R="R --silent --no-restore-data --no-save"
 alias gs='printf "\x1b[1;31m%s\x1b[0m\n" "OMFG WE BOTH KNOW YOU DIDN'\''T MEAN TO USE GHOSTSCRIPT" #'
 alias mf='printf "\x1b[1;31m%s\x1b[0m\n" "OMFG WE BOTH KNOW YOU DIDN'\''T MEAN TO USE METAFONT, WHATEVER TS IS" #'
 
-alias h='GIT_DIR="$HOME/.dotfiles.git" GIT_WORK_TREE="$HOME" '
+alias h='GIT_DIR="$HOME/.config/dotfiles/bare.git" GIT_WORK_TREE="$HOME" '
 # can put dotfiles in home with .git as .dotfiles.git and manage with h
 # alias
 
